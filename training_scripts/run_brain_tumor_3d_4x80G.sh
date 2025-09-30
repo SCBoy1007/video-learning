9948d1f509a54301353bfab5c32133fd1c21475a
SCBoy1007 <chaseshi@hku.hk>
2025-10-01 02:57:19 +0800

    Reduce KL loss coefficient from 1e-2 to 1e-3 to unblock policy updates

--- training_scripts/run_brain_tumor_3d_4x80G.sh
+++ training_scripts/run_brain_tumor_3d_4x80G.sh
@@ -29,7 +29,7 @@ python3 -m verl.trainer.main \
     data.train_files=${TRAIN_DATA} \
     data.val_files=${VAL_DATA} \
     worker.actor.model.model_path=${MODEL_PATH} \
-    worker.actor.kl_loss_coef=1.0e-2 \
+    worker.actor.kl_loss_coef=1.0e-3 \
     worker.actor.optim.lr=3.0e-5 \
     worker.actor.micro_batch_size_per_device_for_update=1 \
     worker.rollout.enable_chunked_prefill=false \
